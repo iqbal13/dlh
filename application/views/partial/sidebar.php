d7b02de065e9346d0425548966ef1b8b5f01c5fd
Maulana Muhammad Iqbal <maulanaiqbal1311@gmail.com>
2017-12-10 14:37:01 +0700

    first

--- application/views/partial/sidebar.php
+++ application/views/partial/sidebar.php
@@ -38,52 +38,7 @@
             
           </ul>
         </li>
-           <li class="treeview">
-          <a href="#">
-            <i class="fa fa-files-o"></i>
-            <span>Standar 4</span>
-           <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="<?php echo base_url() ?>standar4/dosentetap"><i class="fa fa-circle-o"></i> Dosen Tetap Institusi </a></li>
-            <li><a href="<?php echo base_url() ?>standar4/dosentetapdiluar"><i class="fa fa-circle-o"></i>
-Dosen Tetap yang <br/ >keahlianya diluar prodi </a></li>
-            <li><a href="<?php echo base_url() ?>standar4/aktivitasdosentetap"><i class="fa fa-circle-o"></i> Aktivitas Mengajar Dosen Tetap </a></li>
-            <li><a href="<?php echo base_url() ?>standar4/aktivitasdosentetapdiluar"><i class="fa fa-circle-o"></i> Aktivitas Mengajar Dosen Tetap  <br /> Yang keahlianya diluar prodi</a></li>
-            <li><a href="<?php echo base_url() ?>standar4/aktivitasdosentetapdiluar"><i class="fa fa-circle-o"></i>Upaya Peningkatan SDM</a></li>
-
-          </ul>
-        </li>
-            <li class="treeview">
-          <a href="#">
-            <i class="fa fa-files-o"></i>
-            <span>Standar 5</span>
-           <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="<?php echo base_url() ?>standar5/kurikulum"><i class="fa fa-circle-o"></i> Kurikulum </a></li>
-          </ul>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-files-o"></i>
-            <span>Standar 6</span>
-           <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="<?php echo base_url() ?>standar6/penerimaandana"><i class="fa fa-circle-o"></i> Penerimaan Dana </a></li>
-            <li><a href="<?php echo base_url() ?>standar6/penggunaandana"><i class="fa fa-circle-o"></i> Penggunaan Dana </a></li>
-            <li><a href="<?php echo base_url() ?>standar6/danapenelitian"><i class="fa fa-circle-o"></i> Dana untuk Kegiatan Penelitian </a></li>
-            <li><a href="<?php echo base_url() ?>standar6/danapengabdian"><i class="fa fa-circle-o"></i> Dana Untuk Pengabdian </a></li>
-            <li><a href="<?php echo base_url() ?>standar6/prasarana"><i class="fa fa-circle-o"></i> Prasarana </a></li>
-          </ul>
-        </li>
+       
         <li class="treeview">
           <a href="#">
             <i class="fa fa-pie-chart"></i>
