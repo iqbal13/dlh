44619a1047ad2dff0a72c4617112efaa1c15738a
Maulana Muhammad Iqbal <maulanaiqbal1311@gmail.com>
2017-12-10 16:37:51 +0700

    Laporan

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -1,1 +1,21 @@
-100
+<?php
+
+
+		class Laporan extends CI_Controller {
+
+				function __construct(){
+					parent::__construct();
+
+
+				}
+
+				function index($id = 0){
+
+						echo str_replace('-',' ',$id);
+
+						$a = $this->db->get_where('v_laporan_per_tps',array('Kecamatan' => $id))->result_array();
+						echo count($a);
+				}
+
+
+		}
